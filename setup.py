8a4f73109468ea10e3a1e0b35d49454d01564c4f
Giuseppe Ottaviano <giuott@gmail.com>
2009-04-28 23:34:49 +0200

    Added metadata to setup.py for PyPI inclusion

--- setup.py
+++ setup.py
@@ -16,11 +16,22 @@ if __name__ == '__main__':
     from distutils.core import setup
     setup(name=app_name,
           version=app_version,
+          description='Tool to create isolated environments',
+          author='Giuseppe Ottaviano',
+          author_email='giuott@gmail.com',
+          url='http://pypi.python.org/pypi/bpt',
           packages=['bpt',
                     'bpt.ui',
                     'bpt.ui.commands'
                     ],
 	  package_data={'bpt':['env_script.tmpl']},
-          scripts=['box']
+          scripts=['box'],
+          classifiers=['Development Status :: 4 - Beta',
+                       'Environment :: Console',
+                       'Intended Audience :: Developers',
+                       'Intended Audience :: System Administrators',
+                       'License :: OSI Approved :: GNU General Public License (GPL)',
+                       'Operating System :: POSIX',
+                       'Topic :: Software Development :: Build Tools']
           )
 
