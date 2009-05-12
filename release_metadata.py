ee46e82300348b865bfa93ed32cc42612c722347
Giuseppe Ottaviano <giuott@gmail.com>
2009-05-12 16:18:03 +0200

    Cleaned up setup.py

---
+++ release_metadata.py
@@ -0,0 +1,2 @@
+app_name='bpt'
+from bpt import __version__ as app_version
--- setup.py
+++ setup.py
@@ -9,36 +9,34 @@ Basic setup.py for BPT using distutils
 #  the file COPYING, distributed as part of this software.
 #*****************************************************************************
 
-app_name='bpt'
-from bpt import __version__ as app_version
+from release_metadata import *
 
-if __name__ == '__main__':
-    import os
-    from distutils.core import setup
+import os
+from distutils.core import setup
 
-    f = open(os.path.join(os.path.dirname(__file__), 'README'))
-    long_description = f.read().strip()
-    f.close()
+f = open(os.path.join(os.path.dirname(__file__), 'README'))
+long_description = f.read().strip()
+f.close()
 
-    setup(name=app_name,
-          version=app_version,
-          description='Tool to create isolated environments',
-          long_description=long_description,
-          author='Giuseppe Ottaviano',
-          author_email='giuott@gmail.com',
-          url='http://pypi.python.org/pypi/bpt',
-          packages=['bpt',
-                    'bpt.ui',
-                    'bpt.ui.commands'
-                    ],
-	  package_data={'bpt':['env_script.tmpl', 'bpt_base_script.sh']},
-          scripts=['box'],
-          classifiers=['Development Status :: 4 - Beta',
-                       'Environment :: Console',
-                       'Intended Audience :: Developers',
-                       'Intended Audience :: System Administrators',
-                       'License :: OSI Approved :: GNU General Public License (GPL)',
-                       'Operating System :: POSIX',
-                       'Topic :: Software Development :: Build Tools']
-          )
+setup(name=app_name,
+      version=app_version,
+      description='Tool to create isolated environments',
+      long_description=long_description,
+      author='Giuseppe Ottaviano',
+      author_email='giuott@gmail.com',
+      url='http://pypi.python.org/pypi/bpt',
+      packages=['bpt',
+                'bpt.ui',
+                'bpt.ui.commands'
+                ],
+      package_data={'bpt':['env_script.tmpl', 'bpt_base_script.sh']},
+      scripts=['box'],
+      classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: Console',
+                   'Intended Audience :: Developers',
+                   'Intended Audience :: System Administrators',
+                   'License :: OSI Approved :: GNU General Public License (GPL)',
+                   'Operating System :: POSIX',
+                   'Topic :: Software Development :: Build Tools']
+      )
 
