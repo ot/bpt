15503b26d4245c876733b92ca1f14058e1c1ac60
Giuseppe Ottaviano <giuott@gmail.com>
2009-05-29 11:23:47 +0200

    Made pip-box search for installed packages only in the current box. Version 0.5.1

--- bpt/__init__.py
+++ bpt/__init__.py
@@ -9,7 +9,7 @@ Common data for the BPT project
 #  the file COPYING, distributed as part of this software.
 #*****************************************************************************
 
-__version__ = '0.5'
+__version__ = '0.5.1'
 __author__ = 'Giuseppe Ottaviano <giuott@gmail.com>'
 
 import logging
